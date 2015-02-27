87b58f32f76fd0c58b30ad1bea041691aafa5433
John Wilson <johnthewilson@gmail.com>
2015-02-27 04:18:43 -0800

    fr

--- app.js
+++ app.js
@@ -1,73 +1,57 @@
+#!/usr/bin/env node
+
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-
-//Mongo Added
-var mongo = require('mongodb');
-var monk = require('monk');
-var db= monk('localhost:27017/nodertest');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var http = require('http');
+var websocket = require('websocket').server;
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var port = process.env.PORT || '3000';
+app.set('port', port);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(app);
+server.listen(port);
 
-
-//make the db accessible to the router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+function log(msg){
+  console.log((new Date()) + ' ' + msg);
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+server.on('listening', function(){
+  log("Listeing port " + port);
+  log("In your browser open http://127.0.0.1:" + port);
 });
 
+// Create Websocket Serve
+var wsServer = new websocket({
+  httpServer: server,
 
-module.exports = app;
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+  var connection = request.accept('echo-protocol', request.origin);
+
+  log('Connection accepted.');
+
+  connection.on('message', function(message) {
+
+    if (message.type === 'utf8') {
+      log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    }
+
+  });
+
+  connection.on('close', function(reasonCode, description) {
+    log('Peer '+ connection.remoteAddress + ' disconnected.');
+  });
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client.html');
+});
